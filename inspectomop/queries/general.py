93a77b2d0c4c0d255e6d913872120d3cc2524313
Jon Badger <jbadger@wisc.edu>
2018-08-17 16:11:20 -0500

    fixed column filtering bug

--- inspectomop/queries/general.py
+++ inspectomop/queries/general.py
@@ -47,7 +47,7 @@ def concepts_for_concept_ids(concept_ids, inspector,return_columns=None):
     vocabulary = inspector.tables['vocabulary']
     columns = [concept.concept_id, concept.concept_name, concept.concept_code, concept.concept_class_id, concept.standard_concept, concept.vocabulary_id, vocabulary.vocabulary_name]
     if return_columns:
-        columns = filter((lambda x: x in columns, return_columns))
+        columns = list(filter(lambda x: x in columns, return_columns))
     
     statement = select(columns).where(concept.concept_id.in_(list(concept_ids))).where(concept.vocabulary_id == vocabulary.vocabulary_id)
    
@@ -93,7 +93,7 @@ def synonyms_for_concept_ids(concept_ids, inspector,return_columns=None):
     vocabulary = inspector.tables['vocabulary']
     columns = [concept.concept_id, concept_synonym.concept_synonym_name]
     if return_columns:
-        columns = filter((lambda x: x in columns, filter_columns))
+        columns = list(filter(lambda x: x in columns, return_columns))
     statement = select(columns).where(concept.concept_id.in_(list(concept_ids))).where(concept.concept_id == concept_synonym.concept_id).where(concept.vocabulary_id==vocabulary.vocabulary_id)
     return inspector.execute(statement)
 
