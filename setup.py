eef79e19453e4a2a264424177dc5d89b58e991d8
Jon Badger <jbadger@wisc.edu>
2018-08-17 09:36:18 -0500

    first commit for pip dist

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import inspectomop
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+setuptools.setup(
+        #descriptors
+        name='inspectomop',
+        version=inspectomop.__version__,
+        description='Database query tool for the OMOP Common Data Model', 
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='Jonathan Badger',
+        author_email='jonathancbadger@gmail.com',
+        url='https://github.com/jbadger3/inspectomop',
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            ]
+
+        #requirements/dependencies
+        python_requires='>=3.0'
+        
+
+        packages=setuptools.find_packages(),
+        )
+
