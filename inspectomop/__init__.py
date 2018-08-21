f7328c636f1422b617976ac5a09341cb11df06b3
Jonathan Badger <Jon@Jonathans-iMac.local>
2018-08-21 08:41:47 -0500

    fixed setup to require pandas and sqlalchemy as dependencies.  Also fixed proprer determination of package version info

--- inspectomop/__init__.py
+++ inspectomop/__init__.py
@@ -1,4 +1,9 @@
-__version__ = '0.1.0'
+
+with open('inspectomop/VERSION.txt') as fh:
+    version = fh.read().strip()
+
+__version__ = version
+
 from inspectomop.inspector import Inspector
 from inspectomop import queries
-from inspectomop.utils import *
+
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import setuptools
-import inspectomop
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
+
+#load version info
+with open('inspectomop/VERSION.txt') as fh:
+	version = fh.read().strip()
+print(version)
 setuptools.setup(
         #descriptors
         name='inspectomop',
-        version=inspectomop.__version__,
+        version=version,
         description='Database query tool for the OMOP Common Data Model', 
         long_description=long_description,
         long_description_content_type="text/markdown",
@@ -20,7 +24,7 @@ setuptools.setup(
 
         #requirements/dependencies
         python_requires='>=3.0',
-        isntall_requires=['sqlalchemy','pandas'],
+        install_requires=['sqlalchemy','pandas'],
         packages=setuptools.find_packages(),
         )
 
